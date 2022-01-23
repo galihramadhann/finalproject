116b9a61a2480a9c65b66b508df228401139f3a4
galihramadhann <galih123ramadhan@gmail.com>
2022-01-23 19:24:08 +0700

    Menu Utama

--- PENGARSIPAN/menu_utama.Designer.cs
+++ PENGARSIPAN/menu_utama.Designer.cs
@@ -79,6 +79,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.BlueViolet;
 			this.ClientSize = new System.Drawing.Size(580, 321);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.btnkeluar);
